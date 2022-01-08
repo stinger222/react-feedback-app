1b030f11898928dfc971afa44c9c8541af970aa2
= <azazasdfg.com@gmail.com>
2022-01-08 11:21:15 +0300

    Added feedback data storage

---
+++ src/data/feedbackData.js
@@ -0,0 +1,29 @@
+const feedbackData = [
+	{
+		id: 0,
+		rating: 5,
+		description: "Excepteur quis dolore eiusmod do aliqua ullamco non Lorem laborum laboris id adipisicing non. Ullamco cupidatat pariatur officia eu elit deserunt tempor mollit duis proident enim. Nisi nisi aute sint adipisicing ea."
+	},
+	{
+		id: 1,
+		rating: 6,
+		description: "Excepteur quis dolore eiusmod do aliqua ullamco non Lorem laborum laboris id adipisicing non. Ullamco cupidatat pariatur officia eu elit deserunt tempor mollit duis proident enim. Nisi nisi aute sint adipisicing ea."
+	},
+	{
+		id: 2,
+		rating: 5,
+		description: "Excepteur quis dolore eiusmod do aliqua ullamco non Lorem laborum laboris id adipisicing non. Ullamco cupidatat pariatur officia eu elit deserunt tempor mollit duis proident enim. Nisi nisi aute sint adipisicing ea."
+	},
+	{
+		id: 3,
+		rating: 5,
+		description: "Excepteur quis dolore eiusmod do aliqua ullamco non Lorem laborum laboris id adipisicing non. Ullamco cupidatat pariatur officia eu elit deserunt tempor mollit duis proident enim. Nisi nisi aute sint adipisicing ea."
+	},
+	{
+		id: 4,
+		rating: 5,
+		description: "Excepteur quis dolore eiusmod do aliqua ullamco non Lorem laborum laboris id adipisicing non. Ullamco cupidatat pariatur officia eu elit deserunt tempor mollit duis proident enim. Nisi nisi aute sint adipisicing ea."
+	},
+]
+
+export default feedbackData
