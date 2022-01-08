08a9b522c13df0b4ffdad562bb72932872f9c09b
= <azazasdfg.com@gmail.com>
2022-01-08 10:42:23 +0300

    Added Feddback-item component markup

---
+++ src/components/FeedbackItem.js
@@ -0,0 +1,12 @@
+export default function FeedbackItem() {
+	return (
+		<div className="feedback-item">
+			<span className="feedback-item-rating">
+				7
+			</span>
+			<span className="review-text">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laboriosam voluptates at commodi. Consectetur.
+			</span>
+		</div>
+	)
+}
