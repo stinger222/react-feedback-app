da0054bf49deda6f71928a6e5cf5e47801322a1e
= <azazasdfg.com@gmail.com>
2022-01-16 02:42:23 +0300

    Added about page component

---
+++ src/components/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import Card from "../Card";
+
+export default function AboutPage() {
+	return (
+		<Card className={'about'}>
+			<p>
+				This is a simple app where you can send feedback about  products and services.
+			</p>
+		</Card>
+	)
+}
