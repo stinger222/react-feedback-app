3c17c20195b6c770f352146d3e6394ae6c3921d5
= <azazasdfg.com@gmail.com>
2022-01-16 21:15:01 +0300

    Added more information to the AboutPage component

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom";
 import Card from "../Card";
 
 export default function AboutPage() {
 	return (
 		<Card className={'about'}>
+			<h1>
+				About this project
+			</h1>
+			<br/>
 			<p>
 				This is a simple app where you can send feedback about  products and services.
 			</p>
+			<br/>
+			<p>Version: 0.1.0</p>
+			<br/>
+			<Link to='/'>Back To Home</Link>
 		</Card>
 	)
 }
