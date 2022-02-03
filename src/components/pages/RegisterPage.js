9b374dbddc7ed4b7f9de9ee68989327362101df4
= <azazasdfg.com@gmail.com>
2022-02-04 01:04:55 +0300

    Added beta register page

---
+++ src/components/pages/RegisterPage.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+import Button from "../Button"
+import {useState} from 'react'
+
+
+
+export default function RegisterPage() {
+
+	const [login, setLogin] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleLogin = e => {
+		setLogin(e.target.value.trim())
+	}
+
+	const handlePassword = e => {
+		setPassword(e.target.value)
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		axios.post('http://localhost:3001/register', {
+			password,
+			login
+		})
+	}
+
+	return (
+		<form onSubmit={handleSubmit} className="auth-form register">
+			<input onChange={handleLogin} placeholder="login"/>
+			<input onChange={handlePassword} type="password" placeholder="password"/>
+			<Button type="submit">DICK</Button>
+		</form>
+	)
+}
