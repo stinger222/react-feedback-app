6691e3444ecf5059934d1418a5d072001806c120
= <azazasdfg.com@gmail.com>
2022-02-07 22:57:58 +0300

    Added feedbacks model

---
+++ src/models/Feedbacks.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const db = require('../../database')
+
+const Feedbacks = db.define('feedbacks', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true
+	},
+	user_id: {
+		type: Sequelize.INTEGER
+	},
+	text: {
+		type: Sequelize.STRING
+	},
+	rating: {
+		type: Sequelize.INTEGER
+	}
+},{
+	timestamps: false
+})
+
+module.exports = Feedbacks
