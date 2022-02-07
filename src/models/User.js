6d3a2b31f7c2c11ef7a5d1fff85d42ca657b66a2
= <azazasdfg.com@gmail.com>
2022-02-07 22:58:07 +0300

    Added user model

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const db = require('../../database')
+
+const User = db.define('users', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true
+	},
+	login: {
+		type: Sequelize.STRING
+	},
+	password_hash: {
+		type: Sequelize.STRING
+	},
+	created_at: {
+		type: Sequelize.DATE
+	}
+},{
+	timestamps: false
+})
+
+module.exports = User
