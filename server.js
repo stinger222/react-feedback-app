5ba7a38e2016ffda608c746728fd4ff4e8f9ee79
= <azazasdfg.com@gmail.com>
2022-02-07 22:59:37 +0300

    Migration to sequelize ORM

--- server.js
+++ server.js
@@ -1,144 +1,132 @@
+const Feedbacks = require('./src/models/Feedbacks')
+const cookieParser = require('cookie-parser') 
+const User = require('./src/models/User')
 const express = require('express')
 const config = require('config')
-const mysql = require('mysql')
-const cookieParser = require('cookie-parser') 
-const cors = require('cors')
+const bcrypt = require('bcrypt')
 
 const app = express()
-
-const db = mysql.createConnection(config.get('sql-config'))
-db.connect()
-db.query("SET SESSION wait_timeout = 604800")
-
-app.use(cookieParser())
-app.use(express.json())
 
 const allowCrossDomain = function(req,res,next) {
   res.header('Access-Control-Allow-Origin', config.get('client-origin'));
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header("Access-Control-Allow-Headers", "Content-Type")
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
   next();  
 }
-app.use(allowCrossDomain);
 
+const successResponse = (code = 200, message, res) => {
+	res.statusCode = code
+	res.json({
+		errorCode: 0,
+		message
+	})
+	return res
+}	
+
+const errorResponse = (code = 400, message, res) => {
+	res.statusCode = code
+	res.json({
+		errorCode: 1,
+		message: message
+	})
+} 
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(allowCrossDomain)
+
+// Getting user feedbacks
 app.get('/api/user/:user_id', (req, res) => {
-	const query = `SELECT * FROM \`feedbacks\` WHERE user_id = ${req.params.user_id}`
-
- 	db.query(query, (err, data) => {
-		console.log("GET /users/", req.params.user_id);
-
-		if (err) {
-			res.statusCode = 400
-			res.json({error: "Error occurred!"})
-		} else {	
-			res.statusCode = 200
-			res.send(data)
+	Feedbacks.findAll({
+		where: {
+			user_id: req.params.user_id
 		}
-	})
-})
-
-app.get('/api/feedback/:feedback_id', (req, res) => {
-	const query = `SELECT * FROM \`feedbacks\` WHERE feedback_id = ${req.params.feedback_id};`
-
-	db.query(query, (err, data) => {
-		console.log("GET /feedback/", req.params.feedback_id);
-		if (err) {
-			res.statusCode = 400
-			res.json({error: "Error occurred!"})
-		} else {	
-			res.statusCode = 200
-			res.send(data)
-		}
-	})
+	}).then(feedbacks => {
+		res.statusCode = 200
+		res.send(feedbacks)
+	}).catch(err => errorResponse(400, err.message, res))
 })
 
 app.put('/api/feedback/:feedback_id', (req, res) => {
-	const query = `UPDATE \`feedbacks\`
-	SET \`text\` = '${req.body.text}',
-	\`rating\` = '${req.body.rating}'
-	WHERE \`feedbacks\`.\`id\` = ${req.params.feedback_id};`
-	
-	db.query(query, (err) => {
-		console.log("PUT /feedback", req.params.feedback_id)
-		if (err) {
-			res.statusCode = 400
-			res.json({error: "Updating failed!"})
-		} else {	
-			res.statusCode = 200
-			res.json({message: 'Feedback updated successfully!'})
+	const {text, rating} = req.body
+	Feedbacks.update({
+		text,
+		rating,
+	}, {
+		where: {
+			id: req.params.feedback_id
 		}
-	})
+	}).then(() => successResponse(200, 'Feedback updated successfully', res))
+		.catch(err => errorResponse(400, err.message, res))
 })
 
 app.post('/api/feedback', (req, res) => {
-	const query = `INSERT INTO \`feedbacks\`
-	(\`user_id\`, \`text\`, \`rating\`) VALUES ('1', '${req.body.text}', '${req.body.rating}')`
-	
-	db.query(query, (err) => {
-		console.log("POST /feedback")
-		if (err) {
-			res.statusCode = 400
-			res.json({error: "Appending failed!"})
-		} else {	
-			res.statusCode = 201
-			res.json({message: 'Feedback added successfully!'})
-		}
-	})
+	const {text, rating} = req.body
+	Feedbacks.create({
+		user_id: 1,
+		rating,
+		text
+	}).then(() => successResponse(201, 'Feedback created', res))
+		.catch(err => errorResponse(400, err.message, res))
 })
 
+
 app.delete('/api/feedback/:feedback_id', (req, res) => {
-	const query = `DELETE FROM \`feedbacks\` WHERE \`feedbacks\`.\`id\` = ${req.params.feedback_id}`
-	
-	db.query(query, (err) => {
-		console.log("POST /feedback")
-		if (err) {
-			res.statusCode = 400
-			res.json({error: "Deleting failed!"})
-		} else {	
-			res.statusCode = 200
-			res.json({message: 'Feedback deleted successfully!'})
+	Feedbacks.destroy({
+		where: {
+			id: req.params.feedback_id
 		}
-	})
+	}).then(() => successResponse(201, 'Feedback deleted', res))
+		.catch(err => errorResponse(400, err.message, res))
 })
 
 // auth part
 
-app.post("/register", (req, res) => {
-	console.log('Register request');
-	const insertQuery = `INSERT INTO \`users\` (\`login\`, \`password_hash\`)
-	VALUES ('${req.body.login}', '${req.body.password}')`
-
-	const checkQuery = 'SELECT * FROM \`users\` WHERE login = ?'
+app.post("/register", async (req, res) => {
+	console.log('Register request: ', req.body)
 	
-	db.query(checkQuery, [req.body.login], (err, data) => {
-		if (data.length > 0) {
-			res.statusCode = 400
-			res.json({error: "Such user already exists!"})
-		} else {
-			db.query(insertQuery, (err) => {
-				if (err) {
-					res.statusCode = 400
-					res.json({error: "User creating is failed!"})
-				} else {	
-					res.statusCode = 201
-					res.json({message: 'User created successfully!'})
-				}
-			})
-		}
-	})
+	const {login, password} = req.body
+
+	if (password.length < 7) {
+		errorResponse(400, 'Password must be at least 7 characters long', res)
+		return
+	}
+
+	const user = await User.findOne({ where: { login } })
+
+	if (user) {
+		errorResponse(400, 'Such user already exists', res)
+		return
+	}
+
+	if (!user) {
+		const password_hash = await bcrypt.hash(password, 10)
+		User.create({
+			login,
+			password_hash
+		}).then(() => successResponse(201, 'User created', res))
+			.catch(err => errorResponse(500, err.message, res))
+	}
 })
 
-app.post("/login", (req, res) => {
-	db.query('SELECT * FROM `users` WHERE `login` = ? AND `password_hash` = ? LIMIT 1', [req.body.login, req.body.password], (err, users) => {
-		if (users.length === 0) {
-			res.statusCode = 400
-			res.json({error: 'User not found!'})
+app.post("/login", async (req, res) => {
+	const {login, password} = req.body
+
+	const user = await User.findOne({ where: { login } })
+
+	if (!user) errorResponse(400, 'User not found', res)
+
+	if (user) {
+		const match = await bcrypt.compare(password, user.password_hash)
+		if (match) {
+			res.cookie('authorized', 'true', {maxAge: 600000})
+			successResponse(200, 'Logged in', res)
+		} else {
+			errorResponse(400, 'Invalid password', res)
 		}
-		else {
-			res.cookie('authorized', 'true', {maxAge: 600000}).json({message: 'success'})
-		}
-	})
+	}
 })
 
 const port = config.get('server-port')
-app.listen(+port, () => console.log('app has been started on port 3001, kekw'))
+app.listen(+port, () => console.log('App has been started on port', config.get('server-port')))
