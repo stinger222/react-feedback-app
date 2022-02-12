3e8210e70ffaea3a2230e84eee4d399e425fe458
= <azazasdfg.com@gmail.com>
2022-02-13 01:49:05 +0300

    Header.js migrated to TS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
 
-export default function Header({ text }) {
+interface HeaderProps {
+	text: string
+}
+
+export default function Header({ text }: HeaderProps) {
 	return (
 		<>
 			<header>
